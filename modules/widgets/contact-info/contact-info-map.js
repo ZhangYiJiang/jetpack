532c9c532bf51122cbc405b125535ae6c71b4a95
Zhang Yi Jiang <ZhangYiJiang@users.noreply.github.com>
2015-06-03 11:08:20 +0800

    Removed deprecated $(document).on('ready', ...)

--- modules/widgets/contact-info/contact-info-map.js
+++ modules/widgets/contact-info/contact-info-map.js
@@ -1,23 +1,23 @@
 /* global google */
 /* jshint unused:false */
 if (jQuery) {
-	jQuery().ready(function() {
-		jQuery('div.contact-map').each(function(){
+	jQuery( document ).ready(function( $ ) {
+		$('div.contact-map').each( function(){
 			// get lat and lon from hidden input values
-			var lat = jQuery(this).find('.contact-info-map-lat').val(),
-				lon = jQuery(this).find('.contact-info-map-lon').val(),
+			var lat = $( this ).find( '.contact-info-map-lat' ).val(),
+				lon = $( this ).find( '.contact-info-map-lon' ).val(),
 				lat_lon = new google.maps.LatLng( lat, lon ),
 				mapOptions = {
 					zoom: 16,
 					center: lat_lon,
 					mapTypeId: google.maps.MapTypeId.ROADMAP
 				},
-				map = new google.maps.Map(jQuery(this).find('.contact-info-map-canvas')[0], mapOptions),
-				marker = new google.maps.Marker({
+				map = new google.maps.Map( $( this ).find( '.contact-info-map-canvas' )[0], mapOptions ),
+				marker = new google.maps.Marker( {
 					map: map,
 					position: lat_lon
-				});
+				} );
 				
-		});
-	});
+		} );
+	} );
 }
